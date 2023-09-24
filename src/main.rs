856baab345a77aecce07fa3df9d2bde2c4c39dbf
John Allen <john@threedogconsulting.com>
2023-09-24 17:04:16 -0400

    feat: add `listall` command
    Relates to #16

--- src/main.rs
+++ src/main.rs
@@ -55,6 +55,7 @@ fn main() {
         Some(Commands::Shuffle) => mpd.shuffle(),
         Some(Commands::Lsplaylists) => mpd.lsplaylists(),
         Some(Commands::Playlist { name }) => mpd.playlist(name),
+        Some(Commands::Listall { file }) => mpd.listall(file.as_deref()),
         Some(Commands::Repeat { state }) => mpd.repeat(state),
         Some(Commands::Random { state }) => mpd.random(state),
         Some(Commands::Single { state }) => mpd.single(state),
