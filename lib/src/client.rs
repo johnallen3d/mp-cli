bae4ef0d92b621345097bc331979bd0f43746188
John Allen <john@threedogconsulting.com>
2023-12-27 10:38:05 -0500

    feat: add `search` command (#83)
    Relates to #16

--- lib/src/client.rs
+++ lib/src/client.rs
@@ -684,6 +684,27 @@ impl Client {
         self.current_status()
     }
 
+    pub fn search(
+        &mut self,
+        tag: &str,
+        query: &str,
+    ) -> eyre::Result<Option<String>> {
+        let term = mpd::Term::Tag(tag.into());
+        let mut binding = mpd::Query::new();
+        let query = binding.and(term, query);
+
+        let results = self.client.search(query, None)?;
+
+        let files = Listing::from(results);
+
+        let response = match self.format {
+            OutputFormat::Json => serde_json::to_string(&files)?,
+            OutputFormat::Text => files.to_string(),
+        };
+
+        Ok(Some(response))
+    }
+
     pub fn consume(
         &mut self,
         state: Option<OnOff>,
